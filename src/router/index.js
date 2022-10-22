0e3114e37cf39916d3a023036c91e4a47f796ceb
data-88 <andrew1on1@gmail.com>
2022-10-22 13:10:06 +0200

    python gen

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
     name: "DesktopWithPost",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"Welcome to Guy With Camera's Desktop"
     }
   },
   {
@@ -37,7 +37,7 @@ const routes = [
     name: "Desktop",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"Welcom to GWC's computer"
     }
   },
   {
